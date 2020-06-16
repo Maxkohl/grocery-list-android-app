741688d7a54327443ae59e85beae65815b1a3d63
Max Kohl <maxkohl@maxs-mbp.home>
2020-06-16 17:31:46 -0400

    Created onActivityResult function to catch intent response and display it too application

--- app/src/main/java/com/example/grocerylistapp/MainActivity.java
+++ app/src/main/java/com/example/grocerylistapp/MainActivity.java
@@ -1,27 +1,45 @@
 package com.example.grocerylistapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int TEXT_REQUEST = 1;
+
+    TextView item1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        item1 = findViewById(R.id.item1_text);
+
 
     }
 
     public void launchAddItemsActivity(View view) {
         Intent intent = new Intent(this, AddItemsActivity.class);
-
         startActivityForResult(intent, TEXT_REQUEST);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(AddItemsActivity.EXTRA_REPLY);
+                item1.setText(reply);
+                item1.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
