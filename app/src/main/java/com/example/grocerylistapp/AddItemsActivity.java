da520a1d379933c6857694d4bae552714b036b23
Max Kohl <maxkohl@maxs-mbp.home>
2020-06-16 17:10:48 -0400

    Added intent to be passed from AddItemsActivity to MainActivity

--- app/src/main/java/com/example/grocerylistapp/AddItemsActivity.java
+++ app/src/main/java/com/example/grocerylistapp/AddItemsActivity.java
@@ -2,13 +2,33 @@ package com.example.grocerylistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddItemsActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY="com.example.android.grocerylistapp.extra.REPLY";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_items);
+    }
+
+    public void returnItem(View v){
+        Button b = (Button)v;
+        String itemName = b.getText().toString();
+
+        Intent replyIntent = new Intent();
+
+        replyIntent.putExtra(EXTRA_REPLY, itemName);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+
+
+
     }
 }
